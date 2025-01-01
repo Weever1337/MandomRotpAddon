5629d8514f5d415d780d4d2bf41b46669a3a0430
Weever <weever1337@gmail.com>
2025-01-01 02:03:04 +0200

    remapo

--- src/main/java/uk/meow/weever/rotp_mandom/mixin/EntityMixin.java
+++ src/main/java/uk/meow/weever/rotp_mandom/mixin/EntityMixin.java
@@ -1,19 +1,15 @@
 package uk.meow.weever.rotp_mandom.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.item.ArmorStandEntity;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
-import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import uk.meow.weever.rotp_mandom.capability.entity.*;
+
+import net.minecraft.entity.Entity;
+import net.minecraftforge.common.MinecraftForge;
 import uk.meow.weever.rotp_mandom.event.custom.RemoveEntityEvent;
 
-@Mixin(Entity.class)
+@Mixin(value=Entity.class, remap=false)
 public class EntityMixin {
     @Inject(method = "remove(Z)V", at = @At("TAIL"))
     public void remove(boolean keepData, CallbackInfo ci) {
--- src/main/java/uk/meow/weever/rotp_mandom/mixin/WorldMixin.java
+++ src/main/java/uk/meow/weever/rotp_mandom/mixin/WorldMixin.java
@@ -1,5 +1,15 @@
 package uk.meow.weever.rotp_mandom.mixin;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
@@ -8,20 +18,11 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.meow.weever.rotp_mandom.data.global.BlockInventorySaver;
 import uk.meow.weever.rotp_mandom.data.world.BlockData;
 import uk.meow.weever.rotp_mandom.event.custom.SetBlockEvent;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-@Mixin(World.class)
+@Mixin(value=World.class, remap=false)
 public abstract class WorldMixin {
     @Unique
     private static final Map<BlockPos, BlockData.TransferBlockData> mandomRotpAddon$handledPositions = new HashMap<>();
